89d71da7eecaa7925bfa30e9416ace18829e92c8
Go6oto0 <georgi150406@gmail.com>
2024-03-28 08:52:37 +0200

    lev to euro skeleton

--- Calc3.0/Form1.Designer.cs
+++ Calc3.0/Form1.Designer.cs
@@ -34,71 +34,150 @@
             this.result = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.button = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // tempCelsius
             // 
-            this.tempCelsius.Location = new System.Drawing.Point(211, 178);
+            this.tempCelsius.Location = new System.Drawing.Point(158, 145);
+            this.tempCelsius.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tempCelsius.Name = "tempCelsius";
-            this.tempCelsius.Size = new System.Drawing.Size(100, 22);
+            this.tempCelsius.Size = new System.Drawing.Size(76, 20);
             this.tempCelsius.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(317, 181);
+            this.label1.Location = new System.Drawing.Point(238, 147);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(20, 16);
+            this.label1.Size = new System.Drawing.Size(18, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "°C";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(356, 181);
+            this.label2.Location = new System.Drawing.Point(267, 147);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(14, 16);
+            this.label2.Size = new System.Drawing.Size(13, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "=";
             // 
             // result
             // 
             this.result.AutoSize = true;
-            this.result.Location = new System.Drawing.Point(392, 181);
+            this.result.Location = new System.Drawing.Point(294, 147);
+            this.result.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.result.Name = "result";
-            this.result.Size = new System.Drawing.Size(39, 16);
+            this.result.Size = new System.Drawing.Size(32, 13);
             this.result.TabIndex = 3;
             this.result.Text = "result";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(447, 181);
+            this.label4.Location = new System.Drawing.Point(335, 147);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(19, 16);
+            this.label4.Size = new System.Drawing.Size(17, 13);
             this.label4.TabIndex = 4;
             this.label4.Text = "°F";
             // 
             // button
             // 
-            this.button.Location = new System.Drawing.Point(320, 229);
+            this.button.Location = new System.Drawing.Point(240, 186);
+            this.button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button.Name = "button";
-            this.button.Size = new System.Drawing.Size(95, 38);
+            this.button.Size = new System.Drawing.Size(71, 31);
             this.button.TabIndex = 5;
             this.button.Text = "convert";
             this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(240, 76);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 31);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "convert";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(335, 37);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "euro";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(294, 37);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(267, 37);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "=";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(238, 37);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(15, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "lv";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(158, 35);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
+            this.textBox1.TabIndex = 6;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.result);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tempCelsius);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -115,6 +194,12 @@
         private System.Windows.Forms.Label result;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Calc3.0/Form1.cs
+++ Calc3.0/Form1.cs
@@ -26,5 +26,15 @@ namespace Calc3._0
         {
 
         }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
